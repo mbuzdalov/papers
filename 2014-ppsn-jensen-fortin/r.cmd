b644662bab238676a5917cfd8af5a0d66d573841
Maxim Buzdalov <mbuzdalov@gmail.com>
2014-04-01 00:38:48 +0400

    Fixed Windows shell script

--- 2014-ppsn-jensen-fortin/r.cmd
+++ 2014-ppsn-jensen-fortin/r.cmd
@@ -2,23 +2,23 @@
 
 if "%1"=="clean" goto clean
 
-echo "Compiling Java sources... "
-javac -cp . opt\util\FastRandom.java
+echo Compiling Java sources...
+call javac -cp . opt\util\FastRandom.java
 
-echo "Compiling Scala sources..."
-scalac -cp . opt\util\optimized\*.scala opt\util\*.scala opt\test\*.scala opt\types\*.scala opt\multicriteria\nds\*.scala opt\multicriteria\*.scala opt\*.scala
+echo Compiling Scala sources...
+call scalac -cp . opt\util\optimized\*.scala opt\util\*.scala opt\test\*.scala opt\types\*.scala opt\multicriteria\nds\*.scala opt\multicriteria\*.scala opt\*.scala
 
-echo "Running tests..."
-scala -cp . opt.test.All
+echo Running tests...
+call scala -cp . opt.test.All
 
-echo "Running benchmark..."
-scala -cp . opt.test.NonDominatedSortingPerformance
+echo Running benchmark...
+call scala -cp . opt.test.NonDominatedSortingPerformance
 
 goto finish
 
 :clean
 
-echo "Deleting class files... "
+echo Deleting class files...
 del opt\util\optimized\*.class opt\util\*.class opt\test\*.class opt\types\*.class opt\multicriteria\nds\*.class opt\multicriteria\*.class opt\*.class
 
 :finish

8506288b9d9797b337587b01c34bb964b10439c7
Maxim Buzdalov <mbuzdalov@gmail.com>
2015-01-24 02:02:27 +0300

    r.sh fixed to catch up with latest moves

--- 2015-gecco-nsga-ii-ss/r.sh
+++ 2015-gecco-nsga-ii-ss/r.sh
@@ -4,7 +4,7 @@ if [[ "$1" == "clean" ]]; then
     rm -rf classes
 else
     mkdir -p classes
-    javac -Xlint:unchecked -cp src -d classes src/ru/ifmo/steady/{*.java,treap/*.java,util/*.java,inds/*.java}
+    javac -Xlint:unchecked -cp src -d classes src/ru/ifmo/steady/{*.java,util/*.java,inds/*.java}
     java -cp classes ru.ifmo.steady.SolutionStorageTests
 fi
 

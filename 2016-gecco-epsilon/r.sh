94d43fbb294c734af2789830ac18b132ec03d4e4
Maxim Buzdalov <mbuzdalov@gmail.com>
2016-01-31 02:41:01 +0300

    Corrected a wrong Java option in r.sh

--- 2016-gecco-epsilon/r.sh
+++ 2016-gecco-epsilon/r.sh
@@ -9,5 +9,5 @@ echo -n "Compiling... " && javac -cp src -d classes src/ru/ifmo/eps/*.java \
                         && echo "done!"
 echo "Running unit tests..." && java -cp classes ru.ifmo.eps.tests.Tests && \
 echo "Running torture tests..." && java -cp classes ru.ifmo.eps.tests.Torture && \
-echo "Running timing tests..." && taskset 0x01 java -Xmx2G -Xss2G -cp classes ru.ifmo.eps.tests.Timing
+echo "Running timing tests..." && taskset 0x01 java -Xmx2G -Xms2G -cp classes ru.ifmo.eps.tests.Timing
 rm -rf classes

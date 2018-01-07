78a8ba3821cd6343727f4cefaca70c9bb7607daf
Maxim Buzdalov <mbuzdalov@gmail.com>
2018-01-07 14:42:04 +0300

    Simplify test titles and make one of the tests shorter.

--- 2018-cma-es/src/test/scala/ru/ifmo/cma/CMATest.scala
+++ 2018-cma-es/src/test/scala/ru/ifmo/cma/CMATest.scala
@@ -14,8 +14,8 @@ class CMATest extends FlatSpec with Matchers {
     (point - optimum).foreach(_ should (be >= -toleranceSq and be <= toleranceSq))
   }
 
-  "CMA" should "find optimum of a 2D sphere function" in validateCMA(Sphere(DenseVector(2.0, 3.0)), 0)
-     it should "find optimum of a 3D sphere function" in validateCMA(Sphere(DenseVector(4.0, 5.0, 6.0)), 0)
-     it should "find optimum of a 5D sphere function" in validateCMA(Sphere(DenseVector(21.0, 42.0, 15.0, -12.5, -14.2)), 1e-15)
-     it should "find optimum of a 20D sphere function" in validateCMA(Sphere(DenseVector.fill(20, 42.42)), 1e-9)
+  "CMA" should "optimize a 2D sphere function" in validateCMA(Sphere(DenseVector(2.0, 3.0)), 0)
+     it should "optimize a 3D sphere function" in validateCMA(Sphere(DenseVector(4.0, 5.0, 6.0)), 0)
+     it should "optimize a 5D sphere function" in validateCMA(Sphere(DenseVector.fill(5, -11.3)), 1e-15)
+     it should "optimize a 20D sphere function" in validateCMA(Sphere(DenseVector.fill(20, 42.42)), 1e-9)
 }
